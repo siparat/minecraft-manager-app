5536350894a0509ad29858c698ce3a60140c8c0f
siparat <akimov1712@yandex.ru>
2026-02-10 13:56:59 +0300

    feat: add switches ads type & inter delay

--- src/features/toggle-ads-visibility/ui/ToggleAdsVisibility/ToggleAdsVisibility.tsx
+++ src/features/toggle-ads-visibility/ui/ToggleAdsVisibility/ToggleAdsVisibility.tsx
@@ -1,14 +1,18 @@
 import { Switch } from '@/shared/ui';
-import { type JSX } from 'react';
+import { useState, type JSX } from 'react';
 import styles from './ToggleAdsVisibility.module.css';
 import { HTTPError } from 'ky';
 import toast from 'react-hot-toast';
 import { toggleAdsVisibility } from '../..';
 import { useAppStore } from '@/entities/app';
+import { ToggleDetailsAdsVisibility } from './ToggleDetailsAdsVisibility';
+import classNames from 'classnames';
+import ChevronIcon from '@/shared/assets/icons/chevron.svg?react';
 
 export const ToggleAdsVisibility = (): JSX.Element => {
 	const app = useAppStore((state) => state.app);
 	const setApp = useAppStore((state) => state.setApp);
+	const [isOpen, setIsOpen] = useState(false);
 
 	const onSwitch = async (): Promise<void> => {
 		if (!app) {
@@ -31,9 +35,23 @@ export const ToggleAdsVisibility = (): JSX.Element => {
 	}
 
 	return (
-		<div className={styles['wrapper']}>
-			<p className={styles['message']}>Показывать рекламу</p>
-			<Switch value={app.sdk.isAdsEnabled} onSwitch={onSwitch} />
+		<div className={styles['container']}>
+			<div className={styles['wrapper']}>
+				<div className={styles['leftSide']}>
+					<button
+						onClick={() => setIsOpen(!isOpen)}
+						className={classNames(styles['arrowButton'], { [styles['arrowOpen']]: isOpen })}
+						type="button">
+						<ChevronIcon />
+					</button>
+					<p className={styles['message']}>Показывать рекламу</p>
+				</div>
+				<Switch value={app.sdk.isAdsEnabled} onSwitch={onSwitch} />
+			</div>
+
+			<div className={classNames(styles['accordion'], { [styles['accordionOpen']]: isOpen })}>
+				<div className={styles['accordionContent']}>{<ToggleDetailsAdsVisibility sdk={app.sdk} />}</div>
+			</div>
 		</div>
 	);
 };
